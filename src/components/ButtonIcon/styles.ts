185f8b49758c6a0ccdeda2a76b0cbc3fc8c705f7
Matheus Santos <matheus.santos@rotaexata.com.br>
2021-10-16 04:44:40 -0300

    Creating ButtonIcon component #1

---
+++ src/components/ButtonIcon/styles.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+import { theme } from '../../global/styles/theme';
+
+export const styles = StyleSheet.create({
+	container: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		width: '100%',
+		height: 56,
+		backgroundColor: theme.colors.primary,
+		borderRadius: 8
+	},
+	title: {
+		flex: 1,
+		color: theme.colors.heading,
+		fontSize: 15,
+		textAlign: 'center'
+	},
+	iconWrapper: {
+		width: 56,
+		height: 56,
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRightWidth: 1,
+		borderColor: theme.colors.line
+	}
+});
